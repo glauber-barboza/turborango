94f6ffba46d44da62773e20b482577aaffc3ec4b
Arionildo <arionildo6@gmail.com>
2015-06-01 17:52:16 -0300

    Feito Exercicio tema turboRanco

---
+++ src/VisualStudio/turborango/turborango/turborango/Categoria.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TurboRango
+{
+    internal enum Categoria
+    {
+        Comun,
+        CozinhaNatural,
+        CozinhaMexicana,
+        Churrascaria,
+        CozinhaJaponesa,
+        FastFood,
+        Pizzaria
+
+    }
+}
