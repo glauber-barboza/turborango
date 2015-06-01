14fa56aeaf66b56739b44bdd76f05953e3178f9c
Arionildo <arionildo6@gmail.com>
2015-06-01 17:51:47 -0300

    Demonstraçao aula 1

---
+++ src/VisualStudio/AulaDia1/AulaDia1/StringExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaDia1
+{
+    static class StringExtension
+    {
+
+      internal  static string toBacon(this string str)
+        {
+            return str + " bacon!!!";
+        }
+    }
+}
